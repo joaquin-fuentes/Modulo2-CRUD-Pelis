2a04d27936e6bedf314c3383fbf7821e33455bb8
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-04-19 10:15:42 -0300

    traer los datos de localstorage y transformarlos a tipo Pelicula

--- js/administrador.js
+++ js/administrador.js
@@ -14,57 +14,110 @@ const reparto = document.querySelector("#reparto");
 const pais = document.querySelector("#pais");
 const msjFormulario = document.querySelector("#msjFormulario");
 
-const formAdministrarPelicula = document.getElementById("formAdministrarPelicula");
+const formAdministrarPelicula = document.getElementById(
+  "formAdministrarPelicula"
+);
 
-//trabajar las peliculas para que vuelvan a ser un objeto Pelicula.
-let listaPeliculas = JSON.parse(localStorage.getItem("listaPeliculas")) || [];
 
 btnEditar.addEventListener("click", crearPeli);
 btnAgregar.addEventListener("click", mostrarModalPeli);
 formAdministrarPelicula.addEventListener("submit", cargarPelicula);
 
+//trabajar las peliculas para que vuelvan a ser un objeto Pelicula.
+// let listaPeliculas = JSON.parse(localStorage.getItem("listaPeliculas")) || []; ESTO DEVUELVE UN ARRAY DE OBJETOS DE TIPO "OBJET"
+// console.log(listaPeliculas)
+let listaPeliculas = localStorage.getItem("listaPeliculas");
+if (!listaPeliculas) {
+  // si listapeliculas no existe en localstorage
+  listaPeliculas = [];
+} else {
+  // si listaPeliculas tiene datos, quiero trasnformarlo en un array de objetos Pelicula
+  listaPeliculas = JSON.parse(listaPeliculas).map(
+    (pelicula) =>
+      new Pelicula(
+        pelicula.titulo,
+        pelicula.descripcion,
+        pelicula.imagen,
+        pelicula.genero,
+        pelicula.anio,
+        pelicula.duracion,
+        pelicula.pais,
+        pelicula.reparto
+      )
+  );
+  console.log(listaPeliculas)
+}
 
-const modalPelicula = new bootstrap.Modal(document.querySelector("#modalAgregar"));
+
+
+const modalPelicula = new bootstrap.Modal(
+  document.querySelector("#modalAgregar")
+);
 
 function crearPeli() {
-    let nuevaPeli = new Pelicula("Super mario", "algo", "url", "aventura", 2023, "2hs", "EEUU", "-")
-    console.log(nuevaPeli)
-
+  let nuevaPeli = new Pelicula(
+    "Super mario",
+    "algo",
+    "url",
+    "aventura",
+    2023,
+    "2hs",
+    "EEUU",
+    "-"
+  );
+  console.log(nuevaPeli);
 }
 function mostrarModalPeli() {
-    // abrir ventana modal
-    modalPelicula.show();
-    console.log("Aqui vamos a crear una peli");
+  // abrir ventana modal
+  modalPelicula.show();
+  console.log("Aqui vamos a crear una peli");
 }
 
 function cargarPelicula(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    //validar los datos
-    let sumario = sumarioValidaciones(titulo.value, descripcion.value, imagen.value, duracion.value, genero.value, anio.value, pais.value, reparto.value)
-    if (sumario.length === 0) {
-        console.log("Estoy creando la pelicula");
-        //crear la pelicula
-        let nuevaPeli = new Pelicula(titulo.value, descripcion.value, imagen.value, genero.value, anio.value, duracion.value, pais.value, reparto.value)
-        listaPeliculas.push(nuevaPeli);
-        //almacenar la peli en localstorage
+  //validar los datos
+  let sumario = sumarioValidaciones(
+    titulo.value,
+    descripcion.value,
+    imagen.value,
+    duracion.value,
+    genero.value,
+    anio.value,
+    pais.value,
+    reparto.value
+  );
+  if (sumario.length === 0) {
+    console.log("Estoy creando la pelicula");
+    //crear la pelicula
+    let nuevaPeli = new Pelicula(
+      titulo.value,
+      descripcion.value,
+      imagen.value,
+      genero.value,
+      anio.value,
+      duracion.value,
+      pais.value,
+      reparto.value
+    );
+    listaPeliculas.push(nuevaPeli);
+    console.log(listaPeliculas);
+    //almacenar la peli en localstorage
 
-        guardarEnLocalStorage()
-        //limpar el formulario
-        limpiarFormularioPeliculas()
-        //crear modal
-        modalPelicula.hide();
-
-    } else {
-        msjFormulario.className = "alert alert-danger mt-3";
-        msjFormulario.innerHTML = sumario;
-    }
+    guardarEnLocalStorage();
+    //limpar el formulario
+    limpiarFormularioPeliculas();
+    //crear modal
+    modalPelicula.hide();
+  } else {
+    msjFormulario.className = "alert alert-danger mt-3";
+    msjFormulario.innerHTML = sumario;
+  }
 }
 
 function guardarEnLocalStorage() {
-    localStorage.setItem("listaPeliculas", JSON.stringify(listaPeliculas)) //para objetos publicos funciona bien
-
+  localStorage.setItem("listaPeliculas", JSON.stringify(listaPeliculas)); //para objetos publicos funciona bien
 }
 function limpiarFormularioPeliculas() {
-    formAdministrarPelicula.reset()
+  formAdministrarPelicula.reset();
 }
